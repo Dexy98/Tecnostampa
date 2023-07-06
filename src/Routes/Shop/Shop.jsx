b962920afc18a1c2f2a65d7b3729e546b80edd61
Dexy98 <davidecandia@live.it>
2023-07-06 20:06:47 +0200

    puntini

--- src/Routes/Shop/Shop.jsx
+++ src/Routes/Shop/Shop.jsx
@@ -5,7 +5,7 @@ import './Shop.css'
 const Servizi = () => {
   return (
     <div className='mt-[60px]'> 
-      <h1 className=' text-white text-7xl'>work in progress</h1>
+      <h1 className=' text-white text-7xl'>work in progress ...</h1>
     </div>
   )
 }
