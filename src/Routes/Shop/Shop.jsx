b297f97a012cce26be2c56af968b80a5fc1f8d03
Dexy98 <davidecandia@live.it>
2023-07-08 19:26:11 +0200

    shop

--- src/Routes/Shop/Shop.jsx
+++ src/Routes/Shop/Shop.jsx
@@ -2,10 +2,41 @@ import React from 'react'
 import './Shop.css'
 
 
+
+ 
+const handleAddToCart = (productId) => {
+  // Logica per aggiungere il prodotto al carrello
+  console.log(`Aggiungi al carrello: ${productId}`);
+};
+
+
+
 const Servizi = () => {
+  const products = [
+    {
+      id: 1,
+      name: 'Prodotto 1',
+      price: 9.99,
+      image: 'path/to/image1.jpg'
+    },
+    {
+      id: 2,
+      name: 'Prodotto 2',
+      price: 14.99,
+      image: 'path/to/image2.jpg'
+    },
+    // Aggiungi altri prodotti qui...
+  ];
   return (
-    <div className='mt-[60px]'> 
-      <h1 className=' text-white text-7xl'>work in progress ...</h1>
+    <div className="shop-container">
+      {products.map((product) => (
+        <div key={product.id} className="card">
+          <img src={product.image} alt={product.name} />
+          <h3>{product.name}</h3>
+          <p>Prezzo: ${product.price}</p>
+          <button onClick={() => handleAddToCart(product.id)}>Aggiungi al carrello</button>
+        </div>
+      ))}
     </div>
   )
 }
